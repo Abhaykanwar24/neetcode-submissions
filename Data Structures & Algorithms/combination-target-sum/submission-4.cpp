38b00e0279f8775ff251db0d9085ab377735a00c
Abhaykanwar Singh <147252656+Abhaykanwar24@users.noreply.github.com>
2026-05-04 16:18:49 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> curset;
+        vector<vector<int>> comb;
+        DFS(0 ,0 ,  target , nums , curset , comb);
+        return comb;
+    }
+
+private:
+    void DFS(int i ,int sum, int target , vector<int>& nums , vector<int>& curset , vector<vector<int>>& comb){
+        if ( sum == target){
+            comb.push_back(curset);
+            return;
+        }
+        if ( i >= nums.size() || sum > target || nums[i] > target){
+            return;
+        }
+        curset.push_back(nums[i]);
+        DFS(i , nums[i] + sum , target , nums, curset,comb);
+        curset.pop_back();
+
+        DFS(i+1 , sum , target , nums, curset,comb);
+
+    }
+};
