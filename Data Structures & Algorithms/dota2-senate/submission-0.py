3d8a26413e66fdaaa23e4bd921b020f04c9b1aab
Abhaykanwar Singh <147252656+Abhaykanwar24@users.noreply.github.com>
2026-09-18 21:44:20 +0530

    Add: dota2-senate - submission-0

---
+++ Data Structures & Algorithms/dota2-senate/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        senate = list(senate)
+        cnt = 0
+        i = 0
+
+        while i  < len(senate):
+            c = senate[i]
+            if c == 'R':
+                if cnt < 0:
+                    senate.append('D')
+                cnt+=1
+            else:
+                if cnt > 0:
+                    senate.append('R')
+                cnt-=1
+
+            i+=1
+
+        return "Radiant" if cnt > 0 else "Dire"
+
+        
